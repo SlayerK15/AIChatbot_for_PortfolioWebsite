7a8384343a371ab915e09a9d4c899121e1376407
tedtingazuredevopsCICD <gathemanoj@outlook.com>
2025-02-14 20:00:58 +0530

    modified the outputs.td

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,24 @@
-output "ecs_public_ip" {
-  value = aws_ecs_task_definition.chatbot_task.network_configuration[0].assign_public_ip
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.chatbot_cluster.name
 }
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.chatbot_service.name
+}
+
+output "ecs_task_definition_arn" {
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.chatbot_task.arn
+}
+
+output "ecs_public_ip" {
+  description = "The public IP assigned to the ECS service"
+  value       = aws_lb.chatbot_lb.dns_name
+}
+
 output "ecs_public_url" {
-  value = "http://${aws_ecs_task_definition.chatbot_task.network_configuration[0].assign_public_ip}:80"
+  description = "The public URL for accessing the chatbot service"
+  value       = "http://${aws_lb.chatbot_lb.dns_name}:80"
 }
